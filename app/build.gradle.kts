e02324322ec66121235bb34b3d72cb8b7b162a7f
ezhuwx@163.com <ezhuwx@163.com>
2024-04-24 18:34:02 +0800

    1、修复资源文件缺失 2、修复打包配置

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,21 @@
+import java.text.SimpleDateFormat
+import java.util.Date
+import com.android.build.gradle.AppExtension
 plugins {
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidLibiary)
+    //alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
 }
 
 android {
     namespace = "com.ez.widget"
     compileSdk = 34
-
+    version = "1.0.1.01"
     defaultConfig {
-        applicationId = "com.ez.widget"
+        //applicationId = "com.ez.widget"
         minSdk = 21
         //noinspection ExpiredTargetSdkVersion
-        targetSdk = 31
-        versionCode = 10101
-        versionName = "1.0.1"
-
+        lint.targetSdk = 31
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -36,6 +37,20 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    afterEvaluate {
+    //生产文件名称
+    libraryVariants.find { variant -> variant.name.lowercase() == "release" }?.outputs
+        ?.find { output -> output.outputFile.name.endsWith(".aar") }?.let { output ->
+            //包名版本号
+            val packageVersion = "QzWidgetTools_${version}".replace(".", "")
+            //时间后缀
+            val timeSuffix = "${SimpleDateFormat("YYMMdd").format(Date())}.aar"
+            //重命名
+            output.outputFile.renameTo(
+                File(output.outputFile.parent, "${packageVersion}_$timeSuffix")
+            )
+        }
     }
 }
 
