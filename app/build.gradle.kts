c040115d2a019075040b817b45873ce03dd2de08
ezhuwx@163.com <ezhuwx@163.com>
2024-04-28 17:01:22 +0800

    1、新增maven发布脚本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,20 @@
 import java.text.SimpleDateFormat
 import java.util.Date
-import com.android.build.gradle.AppExtension
+
 plugins {
     alias(libs.plugins.androidLibiary)
     //alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.mavenPublish)
 }
 
 android {
     namespace = "com.ez.widget"
     compileSdk = 34
-    version = "1.0.1.01"
+    version = "1.0.1.02"
     defaultConfig {
         //applicationId = "com.ez.widget"
-        minSdk = 21
+        minSdk = 24
         //noinspection ExpiredTargetSdkVersion
         lint.targetSdk = 31
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -39,18 +40,18 @@ android {
         jvmTarget = "1.8"
     }
     afterEvaluate {
-    //生产文件名称
-    libraryVariants.find { variant -> variant.name.lowercase() == "release" }?.outputs
-        ?.find { output -> output.outputFile.name.endsWith(".aar") }?.let { output ->
-            //包名版本号
-            val packageVersion = "QzWidgetTools_${version}".replace(".", "")
-            //时间后缀
-            val timeSuffix = "${SimpleDateFormat("YYMMdd").format(Date())}.aar"
-            //重命名
-            output.outputFile.renameTo(
-                File(output.outputFile.parent, "${packageVersion}_$timeSuffix")
-            )
-        }
+        //生产文件名称
+        libraryVariants.find { variant -> variant.name.lowercase() == "release" }?.outputs
+            ?.find { output -> output.outputFile.name.endsWith(".aar") }?.let { output ->
+                //包名版本号
+                val packageVersion = "QzWidgetTools_${version}".replace(".", "")
+                //时间后缀
+                val timeSuffix = "${SimpleDateFormat("YYMMdd").format(Date())}.aar"
+                //重命名
+                output.outputFile.renameTo(
+                    File(output.outputFile.parent, "${packageVersion}_$timeSuffix")
+                )
+            }
     }
 }
 
@@ -70,3 +71,10 @@ dependencies {
     //图表
     implementation(libs.androidChart)
 }
+//maven publish id
+ext {
+    set("PUBLISH_GROUP_ID", "io.gitee.ezhuwx")
+    set("PUBLISH_ARTIFACT_ID", "QzWidgetTools")
+    set("PUBLISH_VERSION", version)
+}
+apply("${rootProject.projectDir}/publish-maven.gradle")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,13 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.androidLibiary) apply false
     //alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.mavenPublish) apply false
+}
+ext {
+    //Maven新版中央仓库
+    set("SonatypeHostMavenCentral", SonatypeHost.CENTRAL_PORTAL)
 }
